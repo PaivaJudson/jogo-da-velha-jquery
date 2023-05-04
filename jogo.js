557a034c9c8c2b4a982ada7d1cf0ced7e465d0a4
Judson Paiva <judson.paiva@hotmail.com>
2023-05-04 02:23:55 +0100

    validado vencedores - jogo da velha

--- jogo.js
+++ jogo.js
@@ -1,5 +1,21 @@
 var rodada = 1;
 var matriz_jogo = Array(3);
+matriz_jogo['a'] = Array(3);
+matriz_jogo['b'] = Array(3);
+matriz_jogo['c'] = Array(3);
+
+matriz_jogo['a'][1] = 0;
+matriz_jogo['a'][2] = 0;
+matriz_jogo['a'][3] = 0;
+
+matriz_jogo['b'][1] = 0;
+matriz_jogo['b'][2] = 0;
+matriz_jogo['b'][3] = 0;
+
+matriz_jogo['c'][1] = 0;
+matriz_jogo['c'][2] = 0;
+matriz_jogo['c'][3] = 0;
+
 
 $(document).ready( function(){
 
@@ -52,6 +68,67 @@ $(document).ready( function(){
         rodada++;
 
         $('#'+id).css('background-image', icone);
+
+        var linha_coluna = id.split('-');
+        matriz_jogo[linha_coluna[0]][linha_coluna[1]] = ponto;
+        verificarCombinacao();
+    }
+
+
+    function verificarCombinacao(){
+        var pontos = 0;
+        for(var i=1; i<=3; i++){
+            pontos = pontos + matriz_jogo['a'][i];
+        }
+        ganhador(pontos);
+
+        pontos = 0;
+        for(var i=1; i<=3; i++){
+            pontos = pontos + matriz_jogo['b'][i];
+        }
+        ganhador(pontos);
+
+        pontos = 0;
+        for(var i=1; i<=3; i++){
+            pontos = pontos + matriz_jogo['c'][i];
+        }
+        ganhador(pontos);
+
+    
+        for(var l=1; l<=3; l++){
+            pontos = 0;
+            pontos = pontos + matriz_jogo['a'][i];
+            pontos = pontos + matriz_jogo['b'][i];
+            pontos = pontos + matriz_jogo['c'][i];
+
+            ganhador(pontos);
+        }
+
+        pontos = 0;
+        pontos = matriz_jogo['a'][1] + matriz_jogo['b'][2] + matriz_jogo['c'][3];
+        ganhador(pontos);
+
+
+        pontos = 0;
+        pontos = matriz_jogo['a'][3] + matriz_jogo['b'][2] + matriz_jogo['c'][1];
+        ganhador(pontos);
+
+    }
+
+    function ganhador(pontos){
+        
+        if(pontos == -3){
+            var jogador1 = $("#apelido-jogador1").val();
+            alert(jogador1+" é o(a) vencedor(a)");
+            $('.settings').off();
+        }
+        
+        if(pontos == 3){
+            var jogador2 = $("#apelido-jogador2").val();
+            alert(jogador2+" é o(a) vencedor(a)");
+            $('.settings').off();
+        }
+
     }
 
 });
